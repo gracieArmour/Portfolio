86de56e37f7dd96a3ab565891c9b2f2838ef9c4d
Gracie Armour <43764940+gracieArmour@users.noreply.github.com>
2025-09-03 19:27:51 -0700

    Skeleton styling on games page

--- portfolio_app/app/games/page.tsx
+++ portfolio_app/app/games/page.tsx
@@ -2,14 +2,19 @@ import dynamic from "next/dynamic";
 
 export default function Games() {
     const data = [
-        {id:1,name:"Piece Name",desc:"Description goes here",demo:"gamedemocomponent1"},
-        {id:2,name:"Piece Name",desc:"Description goes here",demo:"gamedemocomponent2"}
+        {id:1,name:"Piece 1 Name",desc:"Description 1 goes here",demo:"gamedemocomponent1"},
+        {id:2,name:"Piece 2 Name",desc:"Description 2 goes here",demo:"gamedemocomponent2"},
+        {id:3,name:"Piece 3 Name",desc:"Description 3 goes here",demo:"gamedemocomponent2"},
+        {id:4,name:"Piece 4 Name",desc:"Description 4 goes here",demo:"gamedemocomponent2"},
+        {id:5,name:"Piece 5 Name",desc:"Description 5 goes here",demo:"gamedemocomponent2"},
+        {id:6,name:"Piece 6 Name",desc:"Description 6 goes here",demo:"gamedemocomponent2"},
+        {id:7,name:"Piece 7 Name",desc:"Description 7 goes here",demo:"gamedemocomponent2"}
     ];
 
     return (
-        <main className="px-[2em]">
+        <main className="flex flex-col items-center sm:w-2/3 px-[2em]">
             <h1>Game Development</h1>
-            <ul>
+            <ul className="w-full">
                 {data.map((entry) => {
                     const DemoComponent = dynamic(() =>
                         import(`../../components/${entry.demo}`).then((module) => {

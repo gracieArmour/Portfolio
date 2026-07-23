9551f62ae0c50c105cae604edf7a901eb89717e2
gracieArmour <graciearmour42@gmail.com>
2026-07-23 11:00:33 -0700

    Minor fix to strange hydration bug

--- portfolio_app/components/SmellTheIrisesGameEmbed.tsx
+++ portfolio_app/components/SmellTheIrisesGameEmbed.tsx
@@ -1,5 +1,5 @@
 export default function Demo() {
     return (
-        <iframe src="https://itch.io/embed-upload/18448898?color=333333" width="640" height="640" allowFullScreen><a href="https://graciearmour.itch.io/stop-and-smell-the-irises">Play Stop and Smell the Irises on itch.io</a></iframe>    
+        <iframe src="https://itch.io/embed-upload/18448898?color=333333" width="640" height="640" allowFullScreen><a href="https://graciearmour.itch.io/stop-and-smell-the-irises\">Play Stop and Smell the Irises on itch.io</a></iframe>    
     )
 }
